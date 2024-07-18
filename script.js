55b2ab66b29cb0166a5fa1b43f19a8c40475a5b4
lion92 <38532569+lion92@users.noreply.github.com>
2024-07-18 05:23:28 +0200

    Loisirs

--- script.js
+++ script.js
@@ -8,3 +8,30 @@ document.getElementById("menuburger").addEventListener("click", function() {
         div.classList.add("burger");
     }
 });
+
+let currentSlide = 0;
+
+function showSlide(index) {
+    const carousel = document.querySelector('.carousel');
+    const slides = document.querySelectorAll('.carousel-item');
+    if (index >= slides.length) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide = index;
+    }
+    carousel.style.transform = `translateX(${-currentSlide * 100}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    showSlide(currentSlide);
+});
